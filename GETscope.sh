f23e470de634fa204aa5223d8116cb800bbaae11
Outs1d3r-Net <41843160+Outs1d3r-Net@users.noreply.github.com>
2022-09-05 12:46:26 -0300

    Update GETscope.sh

--- GETscope.sh
+++ GETscope.sh
@@ -6,6 +6,6 @@
 # GET RANGES
 for m in $(route -n| grep -i vpn | awk -F " " '{print $3}'| sort | uniq);do
     for i in $(route -n| grep -i vpn | grep $m|cut -d " " -f 1);do
-        ipcalc $i $m | grep "Network:" | cut -d " " -f 4;done;done
+        ipcalc $i $m | grep "Network:" | cut -d " " -f 4;done
 ;done
 
